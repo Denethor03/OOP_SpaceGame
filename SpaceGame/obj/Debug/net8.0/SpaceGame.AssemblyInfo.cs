7d389874f42feff7043e1fed359577f1394ea286
Mateusz Szych <szychmateusz@student.agh.edu.pl>
2025-06-16 21:56:02 +0200

    Added scan functionality and some minor changes

--- SpaceGame/obj/Debug/net8.0/SpaceGame.AssemblyInfo.cs
+++ SpaceGame/obj/Debug/net8.0/SpaceGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaceGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b55fc9843ef926c4ea9d76ba48d67d080c754e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaceGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaceGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
